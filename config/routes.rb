7cdd79af0ebf0b38aa0cb9be9d4f1fd34b5155ac
Georges987 <ayenigeorgepierre@gmail.com>
2025-04-12 05:12:50 +0100

    adding swagger docker

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  # mount SwaggerUiEngine::Engine, at: '/swagger'
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
